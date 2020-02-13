6f220cd9b2ec54a0e2db3214d89643ff5bcd48b2
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2020-02-12 22:25:17 -0800

    Test hvp calc using pytorch and autograd

---
+++ spinup/algos/pytorch/dppo/test_hvp.py
@@ -0,0 +1,69 @@
+from autograd import numpy as np
+import autograd.numpy.random as npr
+import timeit
+from autograd.test_util import check_grads, check_equivalent
+from autograd import (grad, elementwise_grad, jacobian, value_and_grad,
+                      hessian_tensor_product, hessian, make_hvp,
+                      tensor_jacobian_product, checkpoint, make_jvp,
+                      make_ggnvp, grad_and_aux)
+import torch
+
+npr.seed(1)
+
+def jvp():
+    A = np.random.randn(2, 2)
+    def f(x):
+        return np.dot(A, x)
+    x = np.zeros(2)
+    jvp_f_x = make_jvp(f)(x)
+    print(jvp_f_x(np.array([1, 0])))  # f(0) and first column of f's Jacobian at 0
+    print(jvp_f_x(np.array([0, 1])))  # f(0) and second column of f's Jacobian at 0
+
+
+def hvp():
+    hvp = make_hvp(fun)(a)[0]
+    s = hvp(a)
+    return s
+
+def hessian1():
+    H = hessian(fun)(a)
+    s = np.dot(H, a)
+    return s
+
+
+def torchhvp():
+    L = torch.sum(torch.sin(x))
+    y, = torch.autograd.grad(L, x, create_graph=True, retain_graph=False)
+    w = torch.zeros(y.size(), requires_grad=True)
+    g = torch.autograd.grad(y, x, grad_outputs = w, create_graph = True)
+    r = torch.autograd.grad(g, w, grad_outputs = x, create_graph = False)
+    return r
+
+class net(torch.nn.Module):
+    def __init__(self): #, D_in, H, D_out):
+        super(net, self).__init__()
+
+    def forward(self, x):
+        return torch.sum(torch.sin(x))
+
+def torchwNet():
+    fun = net()
+    L = fun(x)
+    y, = torch.autograd.grad(L, x, create_graph=True, retain_graph=False)
+    w = torch.zeros(y.size(), requires_grad=True)
+    g = torch.autograd.grad(y, x, grad_outputs = w, create_graph = True)
+    r = torch.autograd.grad(g, w, grad_outputs = x, create_graph = False)
+    return r
+
+fun = lambda a: np.sum(np.sin(a))
+for size in range(50, 5000, 50):
+    x = torch.randn(size,)
+    x.requires_grad=True
+    a = x.detach().numpy()
+    #print([timeit.timeit(hvp, number=10),timeit.timeit(hessian1, number=10), timeit.timeit(torchhvp, number=10)])
+    print([timeit.timeit(hvp, number=10), timeit.timeit(torchwNet, number=10)])
+
+print(fun(a))
+print(hvp())
+print(hessian1())
+print(torchhvp())
