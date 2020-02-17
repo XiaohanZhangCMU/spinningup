e40d71a866a68978f7e99964ae826a27c4b39985
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2020-02-16 22:42:41 -0800

    fix vpg and plot

--- spinup/algos/pytorch/vpg/run_LunarLander.py
+++ spinup/algos/pytorch/vpg/run_LunarLander.py
@@ -1,5 +1,5 @@
 from spinup.utils.run_utils import ExperimentGrid
-from spinup import dvpg_pytorch
+from spinup import vpg_pytorch
 import torch
 
 if __name__ == '__main__':
@@ -9,13 +9,12 @@ if __name__ == '__main__':
     parser.add_argument('--num_runs', type=int, default=10)
     args = parser.parse_args()
 
-    eg = ExperimentGrid(name='dvpg_LunarLander-v2')
+    eg = ExperimentGrid(name='vpg_LunarLander-v2')
     eg.add('env_name', 'LunarLander-v2', '', True)
     eg.add('seed', [10*i for i in range(args.num_runs)])
     eg.add('epochs', 500)
-    eg.add('epsilon', [0.01, 0.1, 1, 10])
     eg.add('pi_lr', [3e-4, 3e-3, 3e-2])
     eg.add('steps_per_epoch', 4000)
     eg.add('ac_kwargs:hidden_sizes', [(32,32)], 'hid')
     eg.add('ac_kwargs:activation', [torch.nn.Tanh], '')
-    eg.run(dvpg_pytorch, num_cpu=args.cpu)
+    eg.run(vpg_pytorch, num_cpu=args.cpu)
